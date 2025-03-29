2e855b284b7363073a18cc672295d6204be09e18
Magesh-47 <164760791+Magesh-47@users.noreply.github.com>
2025-03-29 12:18:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+  
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    fadeInElements.forEach(element => observer.observe(element));
+  });
+  
